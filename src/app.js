552c2c8839d6ccbebd11b570f29e55a0c0bb4542
Leo Pham <hongthaipro@gmail.com>
2017-10-08 22:27:23 +0700

    setup folder struture

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+import schema from './graphql';
 
 const app = express();
 app.get("/", (req, res) => {
     res.send({ message: "ok", user: { age: 12 } });
     res.end();
-})
+});
+
+app.use('/graphql',
+    graphqlHTTP(req => ({
+        schema,
+        pretty: true,
+        graphiql: true
+    }))
+);
 
 
 const port = process.env.PORT || 5000;
